f1ec6f0daa508aa58b9ce11e5fa27fde9aac6ec3
William Cui <billcuitop@gmail.com>
2025-09-22 18:08:43 -0400

    small add to ensure seed dir exist

--- main.py
+++ main.py
@@ -167,6 +167,7 @@ if __name__ == "__main__":
 
     if args.encoder != "scratch":
         model_path = os.path.join(MODELDIR, f"seed_{args.seed}", f"{descriptor}.ckpt")
+        os.makedirs(os.path.dirname(model_path), exist_ok=True)
         try:
             if args.encoder in ["simclrpretrain", "metagmvae"]:
                 encoder.load_state_dict(torch.load(model_path))
