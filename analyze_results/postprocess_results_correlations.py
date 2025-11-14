3b186ccd2479a487b3d27370dc94fd35de5ad693
William Cui <billcuitop@gmail.com>
2025-11-13 22:34:33 -0500

    when computing correlation with performance, try taking average of shots instead of show them separately

--- analyze_results/postprocess_results_correlations.py
+++ analyze_results/postprocess_results_correlations.py
@@ -9,8 +9,8 @@ plot_figure = True
 write_table = False
 methods_to_include = ['DRESS', 'Supervised-All', "CACTUS-DC", "CACTUS-DINO"]
 methods_markers = ['o', 's', '^', 'D']
-datasets_to_include = ['smallnorb', 'shapes3d', 'causal3d', 'mpi3dhard', 'celebahair']
-datasets_to_display = ['SmallNORB', 'Shapes3D', 'Causal3D', 'MPI3D-Hard', 'CelebA-Hair']
+datasets_to_include = ['smallnorb', 'shapes3d', 'causal3d', 'mpi3dhard', 'celebahair', 'lfwacrossdomain']
+datasets_to_display = ['SmallNORB', 'Shapes3D', 'Causal3D', 'MPI3D-Hard', 'CelebA-Hair', 'LFWA (Cross Domain)']
 
 if __name__ == "__main__":
     print(f"Processing results...")
@@ -24,6 +24,8 @@ if __name__ == "__main__":
             else:   
                 latex_table += "10-Shot & "
             for ds in datasets_to_include:
+                if ds == "lfwacrossdomain":
+                    continue  # skip this dataset for the table
                 accur_vals_all_methods, diversity_vals_all_methods = [], []
                 for method in methods_to_include:
                     accur_val, diversity_val = ACCURACIES_ALL[method][shot][ds], \
@@ -62,46 +64,54 @@ if __name__ == "__main__":
         
         print(f"Latex table generated and saved to {latex_table_filename} file!")
     if plot_figure:
-        fig, axes = plt.subplots(2, len(datasets_to_include), 
-                                 figsize=(5*len(datasets_to_include), 5*2),
-                                 )
+        n_rows, n_cols = 2, int(len(datasets_to_include) / 2)
+        fig, axes = plt.subplots(n_rows, n_cols, 
+                                 figsize=(5*n_cols, 5*n_rows))
+        fig.suptitle("Few-Shot Performance vs. Diversity Correlations", fontsize=20)
         for ds_idx, ds in enumerate(datasets_to_include):
-            diversity_vals_all_methods = []
-            for method in methods_to_include:
-                diversity_vals_all_methods.append(1-np.mean(DIVERSITIES_ALL[method][ds]))
-            for i, shot in enumerate(['five-shot', "ten-shot"]):
-                ax = axes[i, ds_idx]
-                accur_vals_all_methods = []
-                for method in methods_to_include:
-                    accur_vals_all_methods.append(np.mean(ACCURACIES_ALL[method][shot][ds]))
-                # plot the scatter points
-                for x, y, marker in zip(diversity_vals_all_methods, 
-                                        accur_vals_all_methods, 
-                                        methods_markers):
-                    ax.scatter(x, y, label=method, marker=marker, s=100)
-                # compute and show the correlation
-                correlation = compute_performance_diversity_correlation(
-                                    np.array(accur_vals_all_methods), 
-                                    np.array(diversity_vals_all_methods))
-                ax.text(0.05, 0.85, f"Corr: {correlation:.2f}", 
-                        transform=ax.transAxes, fontsize=18, verticalalignment='top')
-                # fit the line on each shot separately
-                slope, intercept = np.polyfit(diversity_vals_all_methods, accur_vals_all_methods, 1)
-                x_line = np.array([min(diversity_vals_all_methods), max(diversity_vals_all_methods)])
-                y_line = slope * x_line + intercept
-                ax.plot(x_line, y_line, linestyle='--', color='gray')
-                ax.grid(True, alpha=0.3)
+            row_idx, col_idx = ds_idx // n_cols, ds_idx % n_cols
+            ax = axes[row_idx, col_idx]
             # set the axis title and grid
-            axes[0, ds_idx].set_title(f"Dataset: {datasets_to_display[ds_idx]}", fontsize=16)
+            ax.set_title(f"Dataset: {datasets_to_display[ds_idx]}", fontsize=16)
+            ax.grid(True, alpha=0.3)
+
+            diversity_vals_all_methods, accur_vals_all_methods = [], []
+            for method in methods_to_include:
+                if ds == "lfwacrossdomain":
+                    # for cross-domain adaptation, we need the diversity for meta-training set
+                    diversity_vals_all_methods.append(1-np.mean(DIVERSITIES_ALL[method]['celebahair']))
+                else:
+                    diversity_vals_all_methods.append(1-np.mean(DIVERSITIES_ALL[method][ds]))
+                # take average of the performance across different shots
+                accurs_vals_all_shots = []
+                for shot in ['five-shot', 'ten-shot']:
+                    accurs_vals_all_shots.append(np.mean(ACCURACIES_ALL[method][shot][ds]))
+                accur_vals_all_methods.append(np.mean(accurs_vals_all_shots))
+            # plot the scatter points
+            for x, y, marker in zip(diversity_vals_all_methods, 
+                                    accur_vals_all_methods, 
+                                    methods_markers):
+                ax.scatter(x, y, label=method, marker=marker, s=90)
+            # compute and show the correlation
+            correlation = compute_performance_diversity_correlation(
+                                accur_vals_all_methods, 
+                                diversity_vals_all_methods)
+            ax.text(0.05, 0.85, f"Corr: {correlation:.2f}", 
+                    transform=ax.transAxes, fontsize=18, verticalalignment='top')
+            # fit the line
+            slope, intercept = np.polyfit(diversity_vals_all_methods, accur_vals_all_methods, 1)
+            x_line = np.array([min(diversity_vals_all_methods), max(diversity_vals_all_methods)])
+            y_line = slope * x_line + intercept
+            ax.plot(x_line, y_line, linestyle='--', color='gray')
+            
         # shared labels for x and y axes
-        for ax in axes[1, :]:
+        for ax in axes[n_rows-1, :]:
             ax.set_xlabel("Diversity Score", fontsize=14)
         for ax in axes[:, 0]:
             ax.set_ylabel("Few-Shot Accuracy (%)", fontsize=14)
         # create a single legend for the methods on the top right subplot of the figure
         axes[0, -1].legend(methods_to_include, fontsize=12, loc='lower right')
 
-        fig.suptitle("Few-Shot Performance vs. Diversity Correlations", fontsize=20)
         plt.tight_layout()
         # save the figure 
         figure_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
